ba0d69460956bc9ce7cc160a654068b1d42a1e35
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2021-01-30 17:43:08 +0800

    test update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to digitalspbu.</p>
+    <p>Update from code.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
